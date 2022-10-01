14ebb1af15e78a1b7d38ac41a0caa52dee9675d9
Sedana Yoga <9g.yoga27@gmail.com>
2022-10-01 20:59:00 +0800

    add: tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        bwport: {
+          0: '#ffffff',
+          100: '#ececec',
+          200: '#b9b9b9',
+          300: '#ababab',
+          400: '#666666',
+          500: '#3c3c3c',
+        },
+      },
+    },
+  },
+  plugins: [],
+}
